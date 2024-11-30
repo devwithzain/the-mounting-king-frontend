717052648438c3879e81d00aba3a55916a63ac36
devwithzain <ZainSoftwear11@gmail.com>
2024-11-30 06:04:27 +0500

    modified

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function App() {
 		requestAnimationFrame(raf);
 	}, []);
 	return (
-		<div className="w-full h-full">
+		<div className="w-full h-full bg-[url('/bg.png')] bg-center bg-no-repeat bg-cover overflow-x-hidden">
 			<HomeHero />
 			<AboutServices />
 			<HomeServices />
