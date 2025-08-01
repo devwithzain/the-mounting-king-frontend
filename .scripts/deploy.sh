5765c1c603f45bf2c97af2d68403ac35667a3c93
Flow Tech <flowtechnologies01@gmail.com>
2025-08-02 04:55:57 +0500

    modified

---
+++ .scripts/deploy.sh
@@ -0,0 +1,18 @@
+set -e
+
+echo "Deployment started..."
+
+# Pull the latest version of the app
+git pull origin master
+echo "New changes copied to server !"
+
+echo "Installing Dependencies..."
+npm install --yes
+
+echo "Creating Production Build..."
+npm run build
+
+echo "PM2 Reload"
+pm2 reload next-app
+
+echo "Deployment Finished!"
